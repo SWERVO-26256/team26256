1f0c035f932b8feefe536ced7916f7ebfc2efb4b
hethm1heswari-pixel <heth.m1heswari@gmail.com>
2026-05-11 12:47:57 -0400

    sitemap update and vercel analytics test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from '@vercel/analytics/react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -56,6 +56,7 @@ function App() {
   return (
     <Router>
       <MainApp />
+      <Analytics />
     </Router>
   );
 }
